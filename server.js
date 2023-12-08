16a10e83f9fe5ec9fb146e9ce3e81992ed4ded07
kunalBando <144883534+kunalBando@users.noreply.github.com>
2023-12-08 11:04:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -96,6 +96,6 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(port, '0.0.0.0' () =>
+app.listen(port, '0.0.0.0' , () =>
   console.log(`Server is running on http://localhost:${port}`)
 );
