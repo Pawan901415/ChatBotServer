956ac1081e110875af162689f4ac11ba5b25ff1f
kunalBando <144883534+kunalBando@users.noreply.github.com>
2023-12-12 16:16:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://nice-sky-08aa0150f.4.azurestaticapps.net');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
