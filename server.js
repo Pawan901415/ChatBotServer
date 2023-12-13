11376ffe81f2391e7ed0537c416d97c72d49b787
Pawan901415 <122601778+Pawan901415@users.noreply.github.com>
2023-12-13 15:09:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.use((req, res, next) => {
 
 const fetchEntityNames = async () => {
   try {
-    // Replace 'YOUR_DOTNET_API_ENDPOINT' with the actual endpoint of your .NET Core API
     const response = await axios.get(
       "https://featuremarketplacewebapi.azurewebsites.net/api/Entity/GetAllEntities"
     );
@@ -35,12 +34,54 @@ const fetchEntityNames = async () => {
 
     return entityNames;
 
-    // Assuming the API response contains an array of entity names
-    //return response.data;
+   
   } catch (error) {
     throw error;
   }
 };
+
+const fetchFeatureNamesByEntityName = async (entityName) => {
+  try {
+    const apiUrl = `https://featuremarketplacewebapi.azurewebsites.net/api/Feature/GetFeaturesByEntityName/${entityName}`;
+    
+    const response = await axios.get(apiUrl);
+
+    // Extract feature names from the array of feature objects
+    const featureNames = response.data.map((feature) => feature.featureName);
+    return featureNames;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// Function to extract entity name from the user's prompt
+const extractEntityNameFromPrompt = (prompt) => {
+  // Define patterns to match entity-related phrases
+  const entityPatterns = [
+    /(?:get|fetch|show)\s*(?:me)?\s*(?:the)?\s*features\s*(?:of|for|in)?\s*(?:the)?\s*("?.*?"?)\s*entity/i,
+    /(?:features\s*(?:of|for|in)?\s*(?:the)?\s*("?.*?"?)\s*entity)/i,
+    /(?:get|fetch|show)\s*(?:me)?\s*(?:the)?\s*("?.*?"?)\s*entity\s*features/i,
+    /(?:features\s*(?:of|for|in)?\s*(?:the)?\s*("?.*?"?))/i,
+    /(?:list|show)\s*(?:me)?\s*(?:all)?\s*features\s*(?:of|for|in)?\s*(?:the)?\s*("?.*?"?)\s*entity/i,
+    /(?:get|fetch)\s*(?:all)?\s*("?.*?"?)\s*entity\s*features/i,
+    /(?:what\s*are|tell\s*me\s*about)\s*(?:the)?\s*features\s*(?:of|for|in)?\s*(?:the)?\s*("?.*?"?)\s*entity/i,
+  ];
+  
+
+  // Try to match entity patterns in the prompt
+  for (const pattern of entityPatterns) {
+    const match = prompt.match(pattern);
+    if (match && match[1]) {
+      // Extracted entity name
+      return match[1].toLowerCase();
+    }
+  }
+
+  // If no match is found
+  return null;
+};
+
+
 
 app.get("/", async (req, res) => {
   res.status(200).send({
@@ -56,14 +97,29 @@ app.post("/", async (req, res) => {
       res.status(200).send({
         bot: "Hello! How can I help you?",
       });
-    } else 
-    if (
+    } else if (prompt.includes("features") && prompt.includes("entity")) {
+      // Extract entity name from the user's prompt
+      const entityName = extractEntityNameFromPrompt(prompt);
+
+      // Check if entityName is valid
+      if (entityName) {
+        // Fetch feature names for the specified entity
+        const featureNames = await fetchFeatureNamesByEntityName(entityName);
+        res.status(200).send({
+          bot: ` Here are the feature names for the entity "${entityName}": ${featureNames.join(", ")}`,
+        });
+      } else {
+        res.status(200).send({
+          bot: "Please provide a valid entity name to fetch its features.",
+        });
+      }
+    }
+    else if (
       (prompt.includes("add") && prompt.includes("entity")) ||
       (prompt.includes("add") && prompt.includes("feature")) ||
       (prompt.includes("add")  && prompt.includes("entities"))||
       (prompt.includes("add")  && prompt.includes("features"))
     ) {
-      // Fetch entity names from the .NET Core API
 
       res.status(200).send({
         bot: "From the Home page click the add button(to add individual entities) or upload button(as CSV file) to upload your features. Enter the entity name you want to add features to if it exists the features will be added to the existing entity with the same name or enter a unique name to create a new entity to add you features in it.",
@@ -73,27 +129,29 @@ app.post("/", async (req, res) => {
       prompt.includes("available entities") ||
       prompt.includes("entities")
     ) {
-      // Fetch entity names from the .NET Core API
       const entityNames = await fetchEntityNames();      
       res.status(200).send({
         bot: "Here are the available entity names: " + entityNames.join(", "),
       });
-    } else {
-      // Handle other types of queries or prompts
-      // Add more conditional statements as needed
-      const response = await openai.createCompletion({
-        model: "gpt-3.5-turbo-instruct-0914", //"text-davinci-003",
-        prompt: `${prompt}`,
-        temperature: 1,
-        max_tokens: 1024,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-      });
-
+    }  
+    else {
       res.status(200).send({
-        bot: response.data.choices[0].text,
+        bot: "Please provide a valid prompt. You can ask me about how to add entities or features, all the available entities, features in a given entity and much more about our website.",
       });
+      
+      // const response = await openai.createCompletion({
+      //   model: "gpt-3.5-turbo", //"text-davinci-003",
+      //   prompt: `${prompt}`,
+      //   temperature: 1,
+      //   max_tokens: 1024,
+      //   top_p: 1,
+      //   frequency_penalty: 0.5,
+      //   presence_penalty: 0,
+      // });
+
+      // res.status(200).send({
+      //   bot: response.data.choices[0].text,
+      // });
     }
   } catch (error) {
     console.error(error);
